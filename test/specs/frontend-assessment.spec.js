cd7af85fb3476500c3c6733acf485dae24c5c4e4
jburmoodqa <72997378+jburmoodqa@users.noreply.github.com>
2023-05-15 07:28:43 -0700

    Update frontend-assessment.spec.js
    clarification for two fields

--- test/specs/frontend-assessment.spec.js
+++ test/specs/frontend-assessment.spec.js
@@ -21,11 +21,11 @@ describe('QA interview assessment using seleniumeasy.com demo site', () => {
 
 /*
     For the below URL:
-        1. Under the section Two Input Fields, set any value in the "Enter a" field and "Enter b" fields
+        1. Under the section Two Input Fields, set any numeric value in the "Enter a" field and "Enter b" fields
         2. Click 'Get Total' button
-        3. Assert that the "Total a + b =" field field contains the sum of the "a" and "b" field values
+        3. Assert that the "Total a + b =" field field contains the numeric sum of the "a" and "b" field values
 */
-    it('should have the right Single Input Field message', async () => {
+    it('should generate the correct total from Two Input Fields', async () => {
         await browser.url('https://demo.seleniumeasy.com/basic-first-form-demo.html')
         // your test script goes here
 
